a6eefe0ad246b91fdce467f2a85930a9845f3cdb
wesling18 <bermudezwesling@gmail.com>
2025-06-09 10:16:46 -0600

    Cliente

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -12,6 +12,10 @@ export const obtenerClientes= async (req, res) => {
     });
   }
 };
+/////////////////////----------------------------------------------------------------------------------/////////////////////////////
+
+
+
 
 // Obtener un cliente por su ID
 export const obtenerCliente = async (req, res) => {
@@ -27,6 +31,80 @@ export const obtenerCliente = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       mensaje: 'Ha ocurrido un error al leer los datos del cliente.'
+    });
+  }
+};
+
+
+
+/////////////////////----------------------------------------------------------------------------------/////////////////////////////7
+
+
+// Registrar un nuevo cliente
+export const registrarCliente = async (req, res) => {
+  try {
+    const { primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, celular, direccion, cedula } = req.body;
+
+    const [result] = await pool.query(
+      'INSERT INTO clientes (primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, celular, direccion, cedula) VALUES (?, ?, ?, ?, ?, ?, ?)',
+      [primer_nombre, segundo_nombre, primer_apellido, segundo_apellido,celular,direccion, cedula]
+    );
+
+    res.status(201).json({ id_cliente: result.insertId });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al registrar el cliente.',
+      error: error
+    });
+  }
+};
+
+/////////////////////----------------------------------------------------------------------------------/////////////////////////////7
+
+// Eliminar un cliente por su ID
+export const eliminarCliente = async (req, res) => {
+  try {
+    const [result] = await pool.query('DELETE FROM Clientes WHERE id_cliente = ?', [req.params.id]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        mensaje: `Error al eliminar el cliente. El ID ${req.params.id} no fue encontrado.`
+      });
+    }
+
+    res.status(204).send(); // Respuesta sin contenido para indicar éxito
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Ha ocurrido un error al eliminar el cliente.',
+      error: error
+    });
+  }
+};
+/////////////////////----------------------------------------------------------------------------------/////////////////////////////7
+
+
+// Actualizar un cliente por su ID (parcial o completa)
+export const actualizarCliente = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const datos = req.body;
+
+    const [resultado] = await pool.query(
+      'UPDATE Clientes SET ? WHERE id_cliente = ?',
+      [datos, id]
+    );
+
+    if (resultado.affectedRows === 0) {
+      return res.status(404).json({
+        mensaje: `El cliente con ID ${id} no existe.`,
+      });
+    }
+
+    res.status(204).send(); // Respuesta sin contenido para indicar éxito
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'Error al actualizar el cliente.',
+      error: error,
     });
   }
 };
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import {  obtenerClientes, obtenerCliente } from '../controllers/clientes.controller.js';
+import {  obtenerClientes, obtenerCliente, registrarCliente,  eliminarCliente, actualizarCliente } from '../controllers/clientes.controller.js';
+
 
 const router = Router();
 
@@ -8,5 +9,19 @@ router.get('/clientes', obtenerClientes);
 
 // Ruta para obtener un cliente por su ID
 router.get('/cliente/:id', obtenerCliente);
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+//Ruta para Registrar un cliente
+router.post('/registrarclientes', registrarCliente);
+
+
+//Ruta para eliminar un cliente
+router.delete('/eliminarclientes', eliminarCliente);
+
+
+//Ruta para actualizar cliente
+router.patch('/actualizarCliente', actualizarCliente);
+
 
 export default router;
